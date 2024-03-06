456238fb3ddb4afcdc0b0a6282e5b75373d10c10
20A31A4230 <kishorekumar110102@gmail.com>
2024-03-06 22:57:58 +0530

    Todolist

---
+++ TodoList/src/App.js
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "./actions";
+import { useState } from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  const dispatch = useDispatch();
+
+  const [todoTitle, setTodoTitle] = useState("");
+  const handleTodo = () => {
+    if (todoTitle.trim()) {
+      const newTodo = {
+        title: todoTitle,
+      };
+      dispatch(addTodo(newTodo));
+      setTodoTitle("");
+    }
+  };
+
+  return (
+    <div className="container mt-5 w-50">
+      <h3 className="text-primary text-center">Todo app using redux react</h3>
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={() => handleTodo()}>
+          Add
+        </button>
+      </div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
